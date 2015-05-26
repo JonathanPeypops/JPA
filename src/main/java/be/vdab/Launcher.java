b9ae043683585741ea143f7f36d563052aa85343
Jonathan Peypops <jonathanpeypops@msn.com>
2015-05-26 11:26:36 +0200

    Launcher update

--- src/main/java/be/vdab/Launcher.java
+++ src/main/java/be/vdab/Launcher.java
@@ -1,7 +1,20 @@
 package be.vdab;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Launcher {
     public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence
+                .createEntityManagerFactory("RealDolmenPersistenceUnit");
+        EntityManager entityManager =
+                entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
 
     }
 }
