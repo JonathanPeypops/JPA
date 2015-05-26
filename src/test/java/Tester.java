966417f7c2b2310a8a773e1b07b5331381f6421a
Jonathan Peypops <jonathanpeypops@msn.com>
2015-05-26 11:49:02 +0200

    update

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1,13 +1,17 @@
 import be.vdab.Book;
 import be.vdab.BookRepository;
 import be.vdab.BookRepositoryBean;
+import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
+import static junit.framework.Assert.assertEquals;
+
 public class Tester {
     private EntityManager em;
 
+    @Test
     public void testOurLogic() {
         BookRepository bookRepository = new BookRepositoryBean(em);
         List<Book> books = bookRepository.findAllBooks();
